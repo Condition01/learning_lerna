8ea5d8da75143cb77aab30162abbcbafd08b506a
condition01 <brunofc11@gmail.com>
2022-05-05 23:09:44 -0300

    removido components

--- packages/components/lib/components.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = components;
-
-function components() {
-    // TODO
-    // todo2
-}
--- packages/my-project01/app.js
+++ packages/my-project01/app.js
@@ -1,2 +1,3 @@
 console.log("Eu sou o app1")
 console.log("Me publica no NPM")
+console.log("Me publica no NPM")
